580df3d68de5923e12b2108bc705f6f01531632c
Md Shafiuzzaman <md.shafiuzzaman.hira@gmail.com>
2022-10-13 11:28:59 -0700

    Creating new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,22 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private 
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end 
 end
