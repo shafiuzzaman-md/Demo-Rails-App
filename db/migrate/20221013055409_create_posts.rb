9f41769a681ec90147772b3f9f18d80748785df8
Md Shafiuzzaman <md.shafiuzzaman.hira@gmail.com>
2022-10-18 13:42:55 -0700

    User list page

--- db/migrate/20221013055409_create_posts.rb
+++ db/migrate/20221013055409_create_posts.rb
@@ -3,8 +3,9 @@ class CreatePosts < ActiveRecord::Migration[7.0]
     create_table :posts do |t|
       t.string :title
       t.text :body
-
+      t.references :user, index: true, foreign_key: true
       t.timestamps
     end
+    add_index :posts, [:user_id, :created_at]
   end
 end
