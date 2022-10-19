c10e6d1eb9fff81161391e86f7d235007727a857
Md Shafiuzzaman <md.shafiuzzaman.hira@gmail.com>
2022-10-18 23:27:20 -0700

    Post CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,31 @@ class PostsController < ApplicationController
     redirect_to user_path(@user)
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+    	
+  def destroy	
+    Post.find(params[:id]).destroy
+    flash[:success] = "Post deleted"
+    redirect_to users_url
+  end
+
   private
     def post_params
       params.require(:post).permit(:title, :message, DateTime.now(), DateTime.now())
